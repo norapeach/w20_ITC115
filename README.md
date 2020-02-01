15e6849abf32a47f2d4f36a03247b0cd31f84c00
Nora P <veganmuffcakes@gmail.com>
2020-02-01 13:42:41 -0800

    Adds example method of for loop that returns a repeated string

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # w20_ITC115
 First Java programs
 
-### 01/19/20 initial and first commit:
-A simple program using a for loop to output the first 12 Fibonacci numbers to the console
+### 2/01/20 repl Method
+(From Building Java Programs chpt 4 ex. 2)
+Example of a method that accepts a String and a number of repetitions as parameters and then outputs a concatenated repeated String. 
 
 ### 1/25 PowersofN Class
 Example of a static method printPowersOfN that takes two arguments for the base number and exponent. The method runs a for loop to output each power-of value starting at 0 up to the exponent value entered in method call.
+
+### 01/19/20 initial and first commit:
+A simple program using a for loop to output the first 12 Fibonacci numbers to the console
+
+
+
---
+++ Replica.java
@@ -0,0 +1,56 @@
+package conditionals;
+
+//*******************************************************************
+// class Replica --> method repl
+//
+// Building Java Programs c4ex2 example of a method called repl
+// 	that accepts 2 parameters: a String and a integer as the number of times
+//	the string will be repeated. If the repetition parameters is 0 or less
+//  the method will return an empty string.
+//By: Nora P.
+//ITC 115 
+//02.01.20
+//*******************************************************************
+
+import java.util.*; // for Scanner
+
+public class Replica {
+
+	public static void main(String[] args) {
+		giveIntro();
+		
+		// initialize Scanner & prompt user/collect values
+		Scanner console = new Scanner(System.in);
+		System.out.print("Enter a word or phrase: ");
+		String phrase = console.nextLine();
+		System.out.print("Enter the number of times to repeat: ");
+		int number = console.nextInt();
+		
+		// run repl method to output repeated string
+		String rep = repl(phrase, number);
+		System.out.println(rep);
+		
+		//close Scanner
+		console.close();
+	}
+	
+	public static void giveIntro() {
+		System.out.println("This program will ask you for a word or phrase");
+		System.out.println("and then the number of times the word or phrase should");
+		System.out.println("be repeated. Then it will output the phrase replicated");
+	}
+	
+	public static String repl(String phrase, int number) {
+		// check if number is 0 or less
+		if (number <= 0) {
+			return "";
+		} else {
+			String replica = "";
+			for (int i = 1; i <= number; i++) {
+				replica += phrase;
+			}
+			return replica;
+		}
+	}
+
+}
